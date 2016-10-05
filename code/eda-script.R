dfc590306cc9eaed8f82ecae3c1ba944b73d98b0
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-05 16:25:27 -0700

    editing output plots in code/eda-script.R

--- code/eda-script.R
+++ code/eda-script.R
@@ -10,19 +10,25 @@ cat("summary statistics of Sales\n\n")
 sum_Sales
 sink()
 
+
+pdf("../images/histogram-tv.pdf",width=8,height=6)
 hist_TV=hist(dat$TV, ylim = c(0,20), col = "#5679DF"
      , main = "Histogram of TV advertising budget", breaks=15, xlab = "TV advertising budget (in thousands of dollars)")
-dev.copy(pdf,"../images/histogram-tv.pdf",width=8,height=6)
 dev.off()
-dev.copy(png,"../images/histogram-tv.png", width=800, height=600)
-dev.off()
+
+png("../images/histogram-tv.png", width=800, height=600)
+hist_TV=hist(dat$TV, ylim = c(0,20), col = "#5679DF"
+             , main = "Histogram of TV advertising budget", breaks=15, xlab = "TV advertising budget (in thousands of dollars)")
 dev.off()
 
 
+pdf("../images/histogram-sales.pdf",width=8,height=6)
 hist(dat$Sales, xlim = c(0, max(dat$Sales+5)), ylim = c(0, 45), col = "#5679DF"
      , main = "Histogram of Sales", breaks=15, xlab = "Sales (in thousands of units)") 
-dev.copy(pdf,"../images/histogram-sales.pdf", width=8,height=6)
 dev.off()
-dev.copy(png,"../images/histogram-sales.png", width=800, height=600)
+
+png("../images/histogram-sales.png", width=800, height=600)
+hist(dat$Sales, xlim = c(0, max(dat$Sales+5)), ylim = c(0, 45), col = "#5679DF"
+     , main = "Histogram of Sales", breaks=15, xlab = "Sales (in thousands of units)") 
 dev.off()
-dev.off()
+
