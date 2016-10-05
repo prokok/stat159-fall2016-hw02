00233abdde55651773120d063e3e12f3c687d5b2
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-04 17:26:13 -0700

    changed the size of plots in eda-script.R

--- code/eda-script.R
+++ code/eda-script.R
@@ -10,13 +10,19 @@ cat("summary statistics of Sales\n\n")
 sum_Sales
 sink()
 
-hist(dat$TV, ylim = c(0,20)
+hist_TV=hist(dat$TV, ylim = c(0,20)
      , main = "Histogram of TV advertising budget", breaks=15, xlab = "TV advertising budget (in thousands of dollars)")
-##png("../images/histogram-tv.png",width=8,height=6)
-##pdf("../images/histogram-tv.pdf",width=8,height=6)
+dev.copy(pdf,"../images/histogram-tv.pdf",width=8,height=6)
+dev.off()
+dev.copy(png,"../images/histogram-tv.png", width=800, height=600)
+dev.off()
+dev.off()
+
 
 hist(dat$Sales, xlim = c(0, max(dat$Sales+5)), ylim = c(0, 45)
      , main = "Histogram of Sales", breaks=15, xlab = "Sales (in thousands of units)") 
-
-head(dat$TV)
-head(dat$Sales)
+dev.copy(pdf,"../images/histogram-sales.pdf", width=8,height=6)
+dev.off()
+dev.copy(png,"../images/histogram-sales.png", width=800, height=600)
+dev.off()
+dev.off()
