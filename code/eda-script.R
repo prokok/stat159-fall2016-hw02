0090ce8afbde6c193471ec4a2ebface68b646b00
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-04 17:43:00 -0700

    Adding colors in boxplots

--- code/eda-script.R
+++ code/eda-script.R
@@ -10,7 +10,7 @@ cat("summary statistics of Sales\n\n")
 sum_Sales
 sink()
 
-hist_TV=hist(dat$TV, ylim = c(0,20)
+hist_TV=hist(dat$TV, ylim = c(0,20), col = "#5679DF"
      , main = "Histogram of TV advertising budget", breaks=15, xlab = "TV advertising budget (in thousands of dollars)")
 dev.copy(pdf,"../images/histogram-tv.pdf",width=8,height=6)
 dev.off()
@@ -19,7 +19,7 @@ dev.off()
 dev.off()
 
 
-hist(dat$Sales, xlim = c(0, max(dat$Sales+5)), ylim = c(0, 45)
+hist(dat$Sales, xlim = c(0, max(dat$Sales+5)), ylim = c(0, 45), col = "#5679DF"
      , main = "Histogram of Sales", breaks=15, xlab = "Sales (in thousands of units)") 
 dev.copy(pdf,"../images/histogram-sales.pdf", width=8,height=6)
 dev.off()
